311cd4beca5a3c73058db547079f62ff9f562880
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-11-30 10:56:07 +0000

    refactor: simplify AbstractAuthMiddleware by removing unnecessary auth configuration check

--- libs/larascript-http/src/http/base/AbstractAuthMiddleware.ts
+++ libs/larascript-http/src/http/base/AbstractAuthMiddleware.ts
@@ -1,14 +1,9 @@
-import { HttpEnvironment } from "@/http/environment/HttpEnvironment.js";
-import ConfigException from "../exceptions/ConfigException.js";
 import Middleware from "./Middleware.js";
 
-export abstract class AbstractAuthMiddleware<Config extends unknown = unknown> extends Middleware<Config> {
+export abstract class AbstractAuthMiddleware<Config = unknown> extends Middleware<Config> {
 
     constructor() {
         super();
-        if (!HttpEnvironment.getInstance().isAuthConfigured()) {
-            throw new ConfigException('Auth service not configured');
-        }
     }
 
 }
