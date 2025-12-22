da0691ae9f88048871e9e1d178ed07d0996e1bcc
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-12-22 12:09:49 +0000

    refectator: improve ProviderInterface contracts, seperating constructor

--- libs/bootstrap/src/contracts/provider.ts
+++ libs/bootstrap/src/contracts/provider.ts
@@ -1,7 +1,8 @@
 export type ProviderInterface = {
-    new(): ProviderInterface;
     boot(): Promise<void>;
     register(): Promise<void>;
 }
 
-export type ProviderInterfaceInstanceType = InstanceType<ProviderInterface>
+export type ProviderInterfaceConstructor ={ new(): ProviderInterface }
+
+export type ProviderInterfaceInstanceType = InstanceType<ProviderInterfaceConstructor>
