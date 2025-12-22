d6239947a59d4c7e0d18040fee592f2adff3831c
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-12-22 11:23:53 +0000

    refactor: remove legacy kernel/provider files

--- libs/larascript-core/src/exceptions/DependencyExecption.ts
+++
@@ -1,6 +0,0 @@
-export class DependencyExecption extends Error {
-  constructor(message: string = "Dependency Exception") {
-    super(message);
-    this.name = "DependencyExecption";
-  }
-}
--- libs/larascript-core/src/index.ts
+++ libs/larascript-core/src/index.ts
@@ -1,8 +1,6 @@
-export * from "./app/index.js";
 export * from "./base/index.js";
 export * from "./concerns/index.js";
 export * from "./consts/index.js";
 export * from "./exceptions/index.js";
-export * from "./kernel/index.js";
 export * from "./services/index.js";
 
