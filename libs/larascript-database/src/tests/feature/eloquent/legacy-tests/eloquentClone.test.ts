79a7afbed7fb89f03bc218c8a26292932230b7ab
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-12-24 12:15:33 +0000

    refactor: database test files structure change

---
+++ libs/larascript-database/src/tests/feature/eloquent/legacy-tests/eloquentClone.test.ts
@@ -0,0 +1,50 @@
+import { forEveryConnection } from "@/tests/tests-helper/forEveryConnection.js";
+import { queryBuilder, testHelper } from "@/tests/tests-helper/testHelper.js";
+import { describe } from "@jest/globals";
+import TestPeopleModel, { resetPeopleTable } from "./models/TestPeopleModel.js";
+
+describe("eloquent", () => {
+  beforeAll(async () => {
+    await testHelper.testBootApp();
+    await resetPeopleTable();
+  });
+
+  test("test clone query", async () => {
+    await forEveryConnection(async (connection) => {
+      const query = queryBuilder(TestPeopleModel, connection);
+
+      const inserted = await query.clone().insert([
+        {
+          name: "John",
+          age: 25,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Jane",
+          age: 30,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+
+      const restrictedQuery = query.clone().where("age", "=", 25);
+      const restrictedResult = await restrictedQuery.first();
+      expect(restrictedResult?.id).toBe(inserted[0].id);
+      expect(restrictedResult?.name).toBe("John");
+
+      console.log("restricted expression", restrictedQuery.getExpression());
+
+      const everythingQuery = query.clone();
+      const everythingResult = await everythingQuery.get();
+
+      console.log("everything expression", everythingQuery.getExpression());
+
+      expect(everythingResult.count()).toBe(2);
+      expect(everythingResult?.[0]?.id).toBe(inserted[0].id);
+      expect(everythingResult?.[0]?.name).toBe("John");
+      expect(everythingResult?.[1]?.id).toBe(inserted[1].id);
+      expect(everythingResult?.[1]?.name).toBe("Jane");
+    });
+  });
+});
---
+++ libs/larascript-database/src/tests/feature/eloquent/legacy-tests/eloquentOrdering.test.ts
@@ -0,0 +1,58 @@
+import { forEveryConnection } from "@/tests/tests-helper/forEveryConnection.js";
+import { queryBuilder, testHelper } from "@/tests/tests-helper/testHelper.js";
+import { describe } from "@jest/globals";
+import TestPeopleModel, { resetPeopleTable } from "./models/TestPeopleModel.js";
+
+const date2024 = new Date("2024-01-01");
+const date2025 = new Date("2025-01-01");
+
+const resetAndRepopulate = async () => {
+  await resetPeopleTable();
+
+  await forEveryConnection(async (connection) => {
+    await queryBuilder(TestPeopleModel, connection).insert([
+      {
+        name: "Alice",
+        age: 25,
+        createdAt: new Date(),
+        updatedAt: date2024,
+      },
+      {
+        name: "Bob",
+        age: 30,
+        createdAt: new Date(),
+        updatedAt: date2025,
+      },
+    ]);
+  });
+};
+
+describe("eloquent", () => {
+  beforeAll(async () => {
+    await testHelper.testBootApp();
+    await resetAndRepopulate();
+  });
+
+  test("test ordering", async () => {
+    await forEveryConnection(async (connection) => {
+      const query = queryBuilder(TestPeopleModel, connection);
+
+      const resuls = await query.clone().oldest("updatedAt").get();
+      expect(resuls.count()).toBe(2);
+      expect(resuls[0].name).toBe("Alice");
+      expect(resuls[1].name).toBe("Bob");
+
+      const resultsSingleOldest = await query
+        .clone()
+        .oldest("updatedAt")
+        .first();
+      expect(resultsSingleOldest?.name).toBe("Alice");
+
+      const resultsSingleNewest = await query
+        .clone()
+        .latest("updatedAt")
+        .first();
+      expect(resultsSingleNewest?.name).toBe("Bob");
+    });
+  });
+});
