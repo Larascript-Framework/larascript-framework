6f4337efc920205cafbf4841c800c102d1605672
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-12-24 12:15:33 +0000

    refactor: database test files structure change

---
+++ libs/larascript-database/src/tests/feature/eloquent/legacy-tests/models/TestBlogPost.ts
@@ -0,0 +1,41 @@
+import DB from "@/database/services/DB.js";
+import { testHelper } from "@/tests/tests-helper/testHelper.js";
+import { IModelAttributes } from "@larascript-framework/contracts/database/model";
+import { DataTypes, Model } from "sequelize";
+
+const tableName = "testsBlogPosts";
+
+export interface ITestsBlogPostsData extends IModelAttributes {
+  id: string;
+  title: string;
+  rating: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const resetTable = async (
+  connections: string[] = testHelper.getTestConnectionNames(),
+) => {
+  for (const connectionName of connections) {
+    const schema = DB.getInstance()
+      .databaseService()
+      .getAdapter(connectionName)
+      .getSchema();
+    if (await schema.tableExists(tableName)) {
+      await schema.dropTable(tableName);
+    }
+
+    await schema.createTable(tableName, {
+      title: DataTypes.STRING,
+      rating: DataTypes.INTEGER,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    });
+  }
+};
+
+export default class TestBlogPost extends Model<ITestsBlogPostsData> {
+  table = tableName;
+
+  public fields: string[] = ["name", "age", "createdAt", "updatedAt"];
+}
---
+++ libs/larascript-database/src/tests/unit/database/databaseAdapter.test.ts
@@ -0,0 +1,29 @@
+import { IDatabaseService } from "@/database/index.js";
+import DatabaseAdapter from "@/database/services/DatabaseAdapter.js";
+import { describe, expect, test } from "@jest/globals";
+import { MockSQLAdapter } from "./mocks/MockSQLAdapter.js";
+
+describe("Database Adapter", () => {
+  test("getName", () => {
+    expect(DatabaseAdapter.getName(MockSQLAdapter)).toBe("MockSQLAdapter");
+  });
+
+  test("getComposerFileNames", () => {
+    const databaseService = jest.fn().mockImplementation((...args: any[]) => {
+      return {
+        getAllAdapterConstructors: jest
+          .fn()
+          .mockImplementation((...args: any[]) => {
+            return [MockSQLAdapter];
+          }),
+      } as unknown as IDatabaseService;
+    }) as unknown as () => IDatabaseService;
+
+    expect(DatabaseAdapter.getComposerFileNames(databaseService())).toEqual([
+      {
+        fullName: "docker-compose.sql.yml",
+        shortName: "sql",
+      },
+    ]);
+  });
+});
